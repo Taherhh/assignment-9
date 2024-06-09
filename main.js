1a8e90209f6c525e7dce279824c70a136ae32b74
Taher Shabbir <taherihh53@gmail.com>
2024-06-09 03:44:45 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+//assignment no 9
+var favoritedResult = 8;
+var messageResult = "My favorite number: ".concat(favoritedResult);
+console.log(messageResult);
---
+++ main.ts
@@ -0,0 +1,6 @@
+//assignment no 9
+let favoritedResult:number =8;
+
+let messageResult:string = `My favorite number: ${favoritedResult}`;
+
+console.log(messageResult);
